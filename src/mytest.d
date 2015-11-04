672a6a9d3bf9c56b4ba70ddbbdfd1f9afd5276a5
Kingsley Hendrickse <kingsleyhendrickse@me.com>
2015-11-04 20:23:47 +0000

    added my best attempt at conversion of mytest.c to mytest.d

---
+++ src/mytest.d
@@ -0,0 +1,43 @@
+// Include the Ruby headers and goodies
+//#include "ruby.h"
+
+//// Defining a space for information and references about the module to be stored internally
+//VALUE MyTest = Qnil;
+
+//// Prototype for the initialization method - Ruby calls this, not you
+//void Init_mytest();
+
+
+
+//// Prototype for our method 'test1' - methods are prefixed by 'method_' here
+//VALUE method_test1(VALUE self);
+
+//// The initialization method for this module
+//void Init_mytest() {
+//	MyTest = rb_define_module("MyTest");
+//	rb_define_method(MyTest, "test1", method_test1, 0);
+//}
+
+//// Our 'test1' method.. it simply returns a value of '10' for now.
+//VALUE method_test1(VALUE self) {
+//	int x = 10;
+//	return INT2NUM(x);
+//}
+module mytest;
+
+import ruby
+
+alias VALUE(*func_type)();
+extern (C) VALUE DexterModule = 0;
+extern (C) VALUE method_test1();
+
+extern (C) VALUE method_test1(VALUE self){
+	int x = 10;
+	return rb_int2inum(x);
+}
+
+void Init_mytest(VALUE self){
+ MyTest = rb_define_module("MyTest");
+  rb_define_method(MyTest, "test1", method_test1, 0);
+}
+
