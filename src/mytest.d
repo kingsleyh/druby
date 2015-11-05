8e9ebb190207da5b79ffbef1b340c26c21a21d6a
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2015-11-05 16:59:45 +0000

    added mystest

--- src/mytest.d
+++ src/mytest.d
@@ -1,43 +1,33 @@
-// Include the Ruby headers and goodies
-//#include "ruby.h"
-
-//// Defining a space for information and references about the module to be stored internally
-//VALUE MyTest = Qnil;
-
-//// Prototype for the initialization method - Ruby calls this, not you
-//void Init_mytest();
-
-
-
-//// Prototype for our method 'test1' - methods are prefixed by 'method_' here
-//VALUE method_test1(VALUE self);
-
-//// The initialization method for this module
-//void Init_mytest() {
-//	MyTest = rb_define_module("MyTest");
-//	rb_define_method(MyTest, "test1", method_test1, 0);
-//}
-
-//// Our 'test1' method.. it simply returns a value of '10' for now.
-//VALUE method_test1(VALUE self) {
-//	int x = 10;
-//	return INT2NUM(x);
-//}
+// Compiled with:                                                                                                                                                       
+// ./dmd2/linux/bin64/dmd  -L-rpath=`pwd`/dmd2/linux/lib64/ -L-L`pwd`/dmd2/linux/lib64/ -defaultlib=libphobos2.so -fPIC -shared -ofmytest.so src/mytest.d               
+// Run with: irb -r ./mytest.so                                                                                                                                         
 module mytest;
 
-import ruby;
+// Everything in Ruby is a value, which is a pointer under the hood,                                                                                                    
+// but is used as numeric value. IDK what's better here: void* or ulong                                                                                                 
+alias ulong VALUE;
 
-alias func_type = VALUE function();
-extern (C) VALUE DexterModule = 0;
-extern (C) VALUE method_test1();
+// This is a bit tricky: Some methods takes function pointers which are                                                                                                 
+// ANYARGS, which is not easily expressible in D. It could be worked                                                                                                    
+// around with templates.                                                                                                                                               
+// Here we just provide a definition that matches our needs.                                                                                                            
+alias ThisFunc = extern(C) VALUE function(VALUE);
+extern(C) void rb_define_method (VALUE, const(char)*, ThisFunc, int);
 
+// Another of Ruby's definition                                                                                                                                         
+extern(C) VALUE rb_define_module (const(char)*);
+extern(C) VALUE rb_int2inum (int);
+
+
+// ALL functions need to be extern(C)                                                                                                                                   
 extern (C) VALUE method_test1(VALUE self){
-	int x = 10;
-	return rb_int2inum(x);
+    int x = 10;
+    return rb_int2inum(x);
 }
 
-void Init_mytest(VALUE self){
- MyTest = rb_define_module("MyTest");
-  rb_define_method(MyTest, "test1", method_test1, 0);
+// Ruby expects Init_NAME, where "NAME" is the name of the shared object.                                                                                               
+// So the shared object here has to be `mytest.so`                                                                                                                      
+extern(C) void Init_mytest(VALUE self){
+    auto MyTest = rb_define_module("MyTest");
+    rb_define_method(MyTest, "test1".ptr, &method_test1, 0);
 }
-
